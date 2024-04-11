d2c857f0500d58b2ae05c9d7a00d223ff6485438
geovanneaoliveira <geovanne@vortexnet.com.br>
2024-04-11 18:18:36 -0300

    a

---
+++ IA/ConvolutionalNeuralNetwork/cnn.py
@@ -0,0 +1,11 @@
+import numpy as np
+import keras as keras
+#import tensorflow as tf
+from matplotlib import pyplot
+
+
+def main():
+    print("hello world!")
+    
+
+main()
---
+++ pythonProject2/cnn.py
@@ -0,0 +1,93 @@
+# -*- coding: utf-8 -*-
+# BLU8002 - IA
+# Prof. Mauri Ferrandin
+# Exemplo de CNN para um problema de classificação multiclasse
+# Adaptado de: https://keras.io/examples/vision/mnist_convnet/
+# REINSTALL TENSORFLOW
+
+import numpy as np
+from tensorflow import keras
+from matplotlib import pyplot
+
+# Model / data parameters
+num_classes = 10
+input_shape = (28, 28, 1)
+
+# the data, split between train and test sets
+(x_train, y_train), (x_test, y_test) = keras.datasets.mnist.load_data()
+
+
+# summarize loaded dataset
+print('Train: X=%s, y=%s' % (x_train.shape, y_train.shape))
+print('Test: X=%s, y=%s' % (x_train.shape, y_train.shape))
+# plot first few images
+for i in range(9):
+	# define subplot
+	pyplot.subplot(330 + 1 + i)
+	# plot raw pixel data
+	pyplot.imshow(x_train[i], cmap=pyplot.get_cmap('gray'))
+# show the figure
+pyplot.show()
+
+# Scale images to the [0, 1] range
+x_train = x_train.astype("float32") / 255
+x_test = x_test.astype("float32") / 255
+# Make sure images have shape (28, 28, 1)
+x_train = np.expand_dims(x_train, -1)
+x_test = np.expand_dims(x_test, -1)
+print("x_train shape:", x_train.shape)
+print(x_train.shape[0], "train samples")
+print(x_test.shape[0], "test samples")
+
+
+# convert class vectors to binary class matrices
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+model = keras.Sequential(
+    [
+        keras.Input(shape=input_shape),
+        keras.layers.Conv2D(32, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        keras.layers.Conv2D(64, kernel_size=(3, 3), activation="relu"),
+        keras.layers.MaxPooling2D(pool_size=(2, 2)),
+        keras.layers.Flatten(),
+        #layers.Dropout(0.5),
+        keras.layers.Dense(16, activation='sigmoid'),
+        keras.layers.Dense(16, activation='sigmoid'),
+        keras.layers.Dense(num_classes, activation="softmax"),
+    ]
+)
+
+model.summary()
+
+batch_size = 128
+epochs = 15
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics=["accuracy"])
+
+hist = model.fit(x_train, y_train, batch_size=batch_size, epochs=epochs, validation_split=0.1)
+
+score = model.evaluate(x_test, y_test, verbose=0)
+print("Test loss:", score[0])
+print("Test accuracy:", score[1])
+
+import matplotlib.pyplot as plt
+print(hist.history.keys())
+# summarize history for accuracy
+plt.plot(hist.history['accuracy'])
+plt.plot(hist.history['val_accuracy'])
+plt.title('model accuracy')
+plt.ylabel('accuracy')
+plt.xlabel('epoch')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
+
+# summarize history for loss
+plt.plot(hist.history['loss'])
+plt.plot(hist.history['val_loss'])
+plt.title('model loss')
+plt.ylabel('loss')
+plt.xlabel('epoch')
+plt.legend(['train', 'valid'], loc='upper left')
+plt.show()
