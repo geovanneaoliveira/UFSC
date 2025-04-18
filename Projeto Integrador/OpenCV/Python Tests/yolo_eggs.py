f577ee4cd6ecfbd5346132ac920fc45c45294e6e
Geovanne Almeida <geovanne@vortexnet.com.br>
2025-04-18 17:00:06 -0300

    aa

--- Projeto Integrador/OpenCV/Python Tests/yolo_eggs.py
+++ Projeto Integrador/OpenCV/Python Tests/yolo_eggs.py
@@ -8,7 +8,14 @@ from ultralytics import YOLO
 from ultralytics.engine.results import Results, Boxes, Annotator
 
 model = YOLO("models/bestv2.pt", task="detect")
-image_path = "datasets/cvr_egg/test/images/IMG_20241020_102042_jpg.rf.10157a2d2fdd578317fe7b52b2f14f1d.jpg"
+image_path = "datasets/cvr_egg/test/images/IMG_20241024_151433_TIMEBURST4_jpg.rf.6e13f08ef214bd2c13e733a59270b741.jpg"
+
+# datasets\cvr_egg\test\images\IMG_20241030_154905_jpg.rf.eb89f2420f86ae1877e60e09b6c5fb55.jpg
+# datasets\cvr_egg\test\images\IMG_20241030_154554_jpg.rf.4111464a138174e77af06a90bf4648ee.jpg
+# datasets\cvr_egg\test\images\IMG_20241030_154554_jpg.rf.4111464a138174e77af06a90bf4648ee.jpg
+# datasets\cvr_egg\test\images\IMG_20241024_160643_TIMEBURST19_jpg.rf.4a74979d9c65ffc321eb7c4044744e00.jpg
+# datasets\cvr_egg\test\images\IMG_20241027_104709_TIMEBURST8_jpg.rf.fbb7856f1c905321a85eb2db3fed976f.jpg
+# datasets\cvr_egg\valid\images\IMG_20241024_151433_TIMEBURST4_jpg.rf.6e13f08ef214bd2c13e733a59270b741.jpg
 
 img = cv2.imread(image_path)
 
@@ -27,11 +34,11 @@ for r in results:
           
 img = annotator.result()  
 
-result_obj = Results(results)
-boxes_obj = Boxes(results[0].boxes)
+# result_obj = Results(results)
+# boxes_obj = Boxes(results[0].boxes)
 
-print(result_obj)
-print(boxes_obj)
+# print(result_obj)
+# print(boxes_obj)
 
-cv2.imshow('YOLO V8 Detection', img)
+cv2.imshow('YOLO V11 Detection', img)
 cv2.waitKey(0)
