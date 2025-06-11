38d3fee033f99b9b655e3e6d00c4a88fde4e419b
geovanneaoliveira <geovanne@vortexnet.com.br>
2025-06-11 11:53:05 -0300

    aaaa

--- Projeto Integrador/OpenCV/Python Tests/yoloseg.py
+++ Projeto Integrador/OpenCV/Python Tests/yoloseg.py
@@ -2,6 +2,7 @@ from ultralytics import YOLO
 import cv2
 import numpy as np
 import random
+from move_ard import move_motor
 
 def draw_yolov11_masks(image, results, alpha=0.5, draw_boxes=True, draw_labels=True):
     """
@@ -63,8 +64,8 @@ def draw_yolov11_masks(image, results, alpha=0.5, draw_boxes=True, draw_labels=T
     return output
 
 # datasets\cvr_egg2\test\images\IMG_20241024_155952_TIMEBURST15_jpg.rf.75edaed86f44495c5b4fb3959c81f881.jpg
-imgpath = "datasets/cvr_egg2/test/images/IMG_20241024_155952_TIMEBURST15_jpg.rf.75edaed86f44495c5b4fb3959c81f881.jpg"
-
+# imgpath = "datasets/cvr_egg/test/images/IMG_20241024_155952_TIMEBURST15_jpg.rf.75edaed86f44495c5b4fb3959c81f881.jpg"
+imgpath = "datasets/cvr_egg/test/images/IMG_20241024_160643_TIMEBURST19_jpg.rf.4a74979d9c65ffc321eb7c4044744e00.jpg"
 model = YOLO("models/seg50epcvr2.pt")
 image = cv2.imread(imgpath)
 
@@ -85,9 +86,25 @@ for seg, cls_id, conf, box in zip(results.masks.data, results.boxes.cls, results
         "mask": seg.cpu().numpy().astype(np.uint8),
         "box": box.cpu().numpy().tolist()
     })
-print(parsed_results)
+
+results_arr = []
+
+for p_result in parsed_results:
+    results_arr.append(p_result['class'])
+
+if 0 or 2 or 4 in results_arr:
+    print("Improprio")
+    move_motor(200)
+elif 3 in results_arr:
+    print("Proprio")
+    move_motor(300)
+else:
+    print("Consumo")
+    move_motor(400)
+
 # Draw and save output
 output_img = draw_yolov11_masks(image, parsed_results)
 cv2.imwrite("segmented_output.jpg", output_img)
 cv2.imshow('YOLO V11 Detection', output_img)
 cv2.waitKey(0)
+
