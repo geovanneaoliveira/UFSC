3485d25d9318e3f05c8679ceaabcc5c6ba91a174
geovanneaoliveira <geovanne@vortexnet.com.br>
2025-09-10 14:53:37 -0300

    aula 4 e atividade 4

---
+++ POO/ufscPOO/src/aulas/aula3/Main.java
@@ -0,0 +1,10 @@
+package aulas.aula3;
+
+public class Main {
+    public static void main(String[] args) {
+        Pessoa p1 = new Pessoa("123123123", "Vasco da Gama");
+        System.out.println(p1.toString());
+
+        Aluno a1 = new Aluno("321312", "Pablo Vegetti", "21100935", "Automação");
+    }
+}
