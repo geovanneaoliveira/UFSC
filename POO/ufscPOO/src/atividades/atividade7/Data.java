e2cf44f4a204ca199664b9e46a389766bbd007d3
Geovanne Almeida <geovanne@vortexnet.com.br>
2025-10-09 15:15:48 -0300

    atividade 7

---
+++ POO/ufscPOO/src/atividades/atividade7/Data.java
@@ -0,0 +1,18 @@
+package atividades.atividade7;
+
+public class Data {
+    private int dia;
+    private int mes;
+    private int ano;
+
+    public Data(int dia, int mes, int ano) {
+        this.dia = dia;
+        this.mes = mes;
+        this.ano = ano;
+    }
+
+    @Override
+    public String toString() {
+        return dia + "/" + mes + "/" + ano;
+    }
+}
