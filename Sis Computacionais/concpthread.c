616bf8cc9deeb93d1bc5d7e45b3a9c462e0cc564
Geovanne Almeida <geovanne@vortexnet.com.br>
2026-03-31 16:45:41 -0300

    aula sistemas computacionais

---
+++ Sis Computacionais/concpthread.c
@@ -0,0 +1,52 @@
+#include <pthread.h>
+#include <stdio.h>
+
+#define TOTAL 1000
+unsigned int sum = 0;
+
+pthread_mutex_t m = PTHREAD_MUTEX_INITIALIZER;
+
+/* thread */
+void *incr(){
+	unsigned int i;
+	
+	for(i=0;i<TOTAL;i++){
+		pthread_mutex_lock(&m);
+		sum += 1;
+		pthread_mutex_unlock(&m);
+	}
+	
+	return NULL;
+}
+
+int main(){
+
+	pthread_t thread1;
+	pthread_t thread2;
+
+	if(pthread_create(&thread1, NULL, incr, NULL)) {
+		fprintf(stderr, "Error creating thread\n");
+		return 1;
+	}
+	
+	if(pthread_create(&thread2, NULL, incr, NULL)) {
+		fprintf(stderr, "Error creating thread\n");
+		return 1;
+	}
+
+
+	if(pthread_join(thread1, NULL)) {
+		fprintf(stderr, "Error joining thread\n");
+		return 2;
+	}
+
+	if(pthread_join(thread2, NULL)) {
+		fprintf(stderr, "Error joining thread\n");
+		return 2;
+	}
+
+	printf("SUM: %d\n", sum);
+
+	return 0;
+}
+
